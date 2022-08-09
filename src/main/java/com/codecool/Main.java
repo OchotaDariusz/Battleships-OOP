99746fcf0b6cbb2b241c45851d1b506fb8fd728a
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-08-09 10:23:58 +0200

    Battleship(game) class

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,10 +1,12 @@
 package com.codecool;
 
-import com.codecool.battleship.*;
+import com.codecool.battleship.Battleship;
 
 public class Main {
 
     public static void main(String[] args) {
+        Battleship game = new Battleship();
 
+        game.startGame();
     }
 }
--- src/main/java/com/codecool/battleship/Game.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.battleship;
-
-public abstract class Game {
-}
