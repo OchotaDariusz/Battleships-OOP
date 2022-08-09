7b8ef3e229a90439103be23e15bcd8651a218d33
Szymon Tracz <szymontracz1@gmail.com>
2022-08-09 10:59:45 +0200

    display

--- src/main/java/com/codecool/battleship/io/Display.java
+++ src/main/java/com/codecool/battleship/io/Display.java
@@ -1,4 +1,17 @@
 package com.codecool.battleship.io;
 
+import java.util.Scanner;
+
 public class Display {
+    public static void main(String[] args) {
+        System.out.println("""
+                WELCOME TO THE BATTLESHIP GAME.
+                PRESS --> 1. PLAYER VS. PLAYER
+                PRESS --> 2. PLAYER VS. COMPUTER
+                PRESS --> 0. EXIT
+                """);
+        Scanner scanner = new Scanner(System.in);
+        int mode = scanner.nextInt();
+        while (mode !=  1 || )
+    }
 }
