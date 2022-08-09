de325ea6c7247236ca76aa038fe32f06980dca03
Wojciech Prusaczyk <wojciech.prusaczyk1@gmail.com>
2022-08-09 10:09:43 +0200

    ships

--- src/main/java/com/codecool/battleship/ships/Ship.java
+++ src/main/java/com/codecool/battleship/ships/Ship.java
@@ -1,4 +1,14 @@
 package com.codecool.battleship.ships;
 
+import com.codecool.battleship.board.Square;
+
+import java.util.List;
+
 public class Ship {
+    private List <Square> coords;
+
+    public List<Square> getCoords() {
+        return coords;
+    }
+
 }
