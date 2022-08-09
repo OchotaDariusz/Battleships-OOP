90dc9e38fd8d45946bb0e45b34411384f4c93972
Iwona Zygma <iwona.zygma@gmail.com>
2022-08-09 10:59:48 +0200

    Board

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,10 +1,30 @@
 package com.codecool;
 
 import com.codecool.battleship.*;
+import com.codecool.battleship.board.Board;
+import com.codecool.battleship.board.Square;
+import com.codecool.battleship.board.SquareStatus;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Board b = new Board();
+        System.out.println("New Ship");
+        System.out.println(b.shipPlace(1,1));
+        System.out.println(b.shipCommit());
+        System.out.println("\n\n");
+
+        System.out.println("New Ship");
+        System.out.println(b.shipPlace(1,1));
+        System.out.println(b.shipPlace(1,2));
+        System.out.println(b.shipCommit());
+        System.out.println("\n\n");
+
+        System.out.println("New Ship");
+        System.out.println(b.shipPlace(5,5));
+        System.out.println(b.shipPlace(6,5));
+        System.out.println(b.shipCommit());
+        System.out.println("\n\n");
     }
 }
--- src/main/java/com/codecool/battleship/board/Square.java
+++ src/main/java/com/codecool/battleship/board/Square.java
@@ -1,4 +1,50 @@
 package com.codecool.battleship.board;
 
 public class Square {
+    private int X;
+    private int Y;
+    private Status SquareStatus;
+
+    public enum Status {
+        S_EMPTY,
+        S_SHIP,
+        S_HIT,
+        S_MISS;
+
+        char GetCharacter() {
+            if (this == Status.S_SHIP) {
+                return '*';
+            }
+            if (this == Status.S_HIT) {
+                return 'x';
+            }
+            if (this == Status.S_MISS) {
+                return 'o';
+            }
+            return ' ';
+        }
+    }
+
+    public Status getSquareStatus() {
+        return SquareStatus;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public Square(int x, int y, Status status) {
+        X = x;
+        Y = y;
+        SquareStatus = status;
+    }
+
+    @Override
+    public String toString() {
+        return Character.toString(this.SquareStatus.GetCharacter());
+    }
 }
